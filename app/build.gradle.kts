4fdf9e0d31b27c8ce5e81eb2f0010c6be931a82d
Feor <mistilplay558@gmail.com>
2026-02-02 15:34:04 +0500

    Add Checkstyle configuration and Gradle wrapper files

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+val checkstyleVersion = "12.1.1"
+val lombokVersion = "1.18.42"
+val junitBomVersion = "5.10.0"
+
+plugins {
+    application
+    jacoco
+    id("checkstyle")
+    id("com.github.ben-manes.versions") version "0.53.0"
+    id("org.sonarqube") version "7.1.0.6387"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+checkstyle {
+    toolVersion = checkstyleVersion
+    configFile = file("config/checkstyle/checkstyle.xml")
+    maxWarnings = 0
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Feoor_java-project-78")
+        property("sonar.organization", "feoor")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // checkstyle
+    implementation("com.puppycrawl.tools:checkstyle:$checkstyleVersion")
+
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
+    testImplementation(platform("org.junit:junit-bom:$junitBomVersion"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("report/jacoco/jacocoHtml"))
+    }
+}
+
+tasks.withType(Checkstyle::class) {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
