bc2f274e5ca735120526f118e533c4ce0d19c758
Feor <mistilplay558@gmail.com>
2026-02-05 19:15:15 +0500

    update: Add checkNumberScheme method to validate number schema with required, positive, and range checks

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class App {
-    public static void main(String[] args) {
+    public static void checkStringScheme() {
         var v = new Validator();
 
         var schema = v.string();
@@ -28,5 +28,39 @@ public class App {
         // то последний имеет приоритет (перетирает предыдущий)
         var schema1 = v.string();
         schema1.minLength(10).minLength(4).isValid("Hexlet"); // true
+    }
+
+    public static void checkNumberScheme() {
+        var v = new Validator();
+
+        var schema = v.number();
+
+        schema.isValid(5); // true
+
+        // Пока не вызван метод required(), null считается валидным
+        schema.isValid(null); // true
+        schema.positive().isValid(null); // true
+
+        schema.required();
+
+        schema.isValid(null); // false
+        schema.isValid(10); // true
+
+        // Потому что ранее мы вызвали метод positive()
+        schema.isValid(-10); // false
+        //  Ноль — не положительное число
+        schema.isValid(0); // false
+
+        schema.range(5, 10);
+
+        schema.isValid(5); // true
+        schema.isValid(10); // true
+        schema.isValid(4); // false
+        schema.isValid(11); // false
+    }
+
+    public static void main(String[] args) {
+        checkStringScheme();
+        checkNumberScheme();
     }
 }
